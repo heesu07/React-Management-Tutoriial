29efac85f9a52492432da84d620246b3588bb933
Daniel <heesu07@gmail.com>
2021-03-31 18:09:38 +1300

    Add getCustomer REST API module

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
-//import { makeStyles, createStyles, withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
   root:{
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.width * 3,
     overflowX: 'auto',
   },
   table:{
@@ -24,29 +22,19 @@ const styles = theme => ({
 
 function App(props) {
   const {classes} = props;
-  const [customers, setCustomers] = useState([
-    { id:0,
-      image: 'https://placeimg.com/128/128/0',
-      name:'Jenny',
-      dob: '060522',
-      gender: 'Female',
-      job:'Student',
-    },
-    { id:1,
-      image: 'https://placeimg.com/128/128/1',
-      name:'Daniel',
-      dob: '720707',
-      gender: 'Male',
-      job:'Student',
-    },
-    { id:2,
-      image: 'https://placeimg.com/128/128/2',
-      name:'Nora',
-      dob: '770707',
-      gender: 'Female',
-      job:'Caregiver',
-    },
-  ]);
+  const [customers, setCustomers] = useState(null);
+
+  const getData = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  };
+
+  useEffect(() => {    
+    getData()
+    .then(res => setCustomers(res))
+    .catch(console.log);
+  }, [customers]);
 
   return (
     
--- server.js
+++ server.js
@@ -3,10 +3,35 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res) => {
-  res.send({message: "Hello Express!"});
+app.get('/api/customers', (req,res) => {
+  res.send(
+    [
+      { id:0,
+        image: 'https://placeimg.com/64/64/0',
+        name:'Jenny',
+        dob: '060522',
+        gender: 'Female',
+        job:'Student',
+      },
+      { id:1,
+        image: 'https://placeimg.com/64/64/1',
+        name:'Daniel',
+        dob: '720707',
+        gender: 'Male',
+        job:'Student',
+      },
+      { id:2,
+        image: 'https://placeimg.com/64/64/2',
+        name:'Nora',
+        dob: '770707',
+        gender: 'Female',
+        job:'Caregiver',
+      },
+    ]
+  );
 });
+
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
